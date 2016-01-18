dbca93ee21c8214ff0795837fa2cdcb9b18b2e79
Louy <louy08@gmail.com>
2016-01-18 11:19:46 +0200

    setup tests

---
+++ test/index.ts
@@ -0,0 +1,5 @@
+import {expect} from 'chai';
+
+describe('redux-async-render', () => {
+  it('awaits promises');
+});
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,6 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="redux/redux.d.ts" />
 /// <reference path="bluebird/bluebird.d.ts" />
+/// <reference path="assertion-error/assertion-error.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
